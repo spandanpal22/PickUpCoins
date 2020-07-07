a7ba3d20d12359e413a4f6193e6920e69daf7e45
Spandan Pal <2spandan2@gmail.com>
2020-07-07 17:11:04 +0530

    initial commit

---
+++ Assets/Scripts/PickUpItem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class PickUpItem : MonoBehaviour
+{
+    public GameObject winMsg;
+    public GameObject scoreMsg;
+    // public GameObject coin;
+
+    public int score=0;
+
+
+    void Update()
+    {
+       RaycastHit hit;
+
+       if(Physics.Raycast(transform.position, transform.forward, out hit, 2f)) 
+       {
+           if(hit.transform.GetComponent<Coin>().isCoin==true && Input.GetKey("e"))
+           {
+               score++;
+               String sc="Score ";
+               sc += score.ToString();
+               scoreMsg.GetComponent<Text>().text=sc;
+                Debug.Log(sc);
+               Destroy(hit.transform.gameObject);
+           }
+       } 
+
+       if(score == 10)
+       {
+        //    win
+        GetComponent<PlayerMove>().enabled = false;
+        winMsg.SetActive(true);
+       }
+
+       Debug.Log(score);
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public float mouseSensX = 1f;
+    public float mouseSensY = 1f;
+
+    float verticalLookRotation;
+
+    Transform mainCam;
+
+    void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        mainCam = Camera.main.transform;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mouseSensX);
+        verticalLookRotation += Input.GetAxis("Mouse Y") * mouseSensY;
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -80, 80);
+        mainCam.localEulerAngles = Vector3.left * verticalLookRotation;
+
+        // Input from user
+        if(Input.GetKey("w"))
+        {
+            transform.position += transform.forward * Time.deltaTime * moveSpeed;
+        }
+        if(Input.GetKey("s"))
+        {
+            transform.position -= transform.forward * Time.deltaTime * moveSpeed;
+        }
+        if(Input.GetKey("d"))
+        {
+            transform.position += transform.right * Time.deltaTime * moveSpeed;
+        }
+        if(Input.GetKey("a"))
+        {
+            transform.position -= transform.right * Time.deltaTime * moveSpeed;
+        }
+        
+    }
+}
